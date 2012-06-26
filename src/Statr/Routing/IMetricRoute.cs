c56e61863737d865798ad98c018b7df11eb67598
jonnii <me@jonnii.com>
2012-06-25 22:09:55 -0400

    add some docs to metric route interface

--- src/Statr/Routing/IMetricRoute.cs
+++ src/Statr/Routing/IMetricRoute.cs
@@ -4,8 +4,20 @@ namespace Statr.Routing
 {
     public interface IMetricRoute : IDisposable
     {
+        /// <summary>
+        /// The data point generated event is raised when metrics have been 
+        /// aggregated.
+        /// </summary>
+        event EventHandler<DataPointEventArgs> DataPointGenerated;
+
+        /// <summary>
+        /// The name of this metric route
+        /// </summary>
         string MetricName { get; }
 
+        /// <summary>
+        /// Pushes a metric into this metric route
+        /// </summary>
         void Push(Metric metric);
     }
 }
