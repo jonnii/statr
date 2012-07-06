a9f8eec22b80f315c2ea4d62a9d1fdfa51127dce
jonnii <me@jonnii.com>
2012-07-06 14:16:39 -0400

    docs for metric router

--- src/Statr/Routing/IMetricRouter.cs
+++ src/Statr/Routing/IMetricRouter.cs
@@ -1,9 +1,20 @@
 namespace Statr.Routing
 {
+    /// <summary>
+    /// The metric router is responsible for taking a metric and routing it to
+    /// the correct metric route.
+    /// </summary>
     public interface IMetricRouter
     {
+        /// <summary>
+        /// The number of metrics processed so far
+        /// </summary>
         ulong NumProcessedMetrics { get; }
 
+        /// <summary>
+        /// Route a metric
+        /// </summary>
+        /// <param name="metric">The metric to route</param>
         void Route(Metric metric);
     }
 }
