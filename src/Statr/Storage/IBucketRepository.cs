92139997863d2ba0fb31de81353063fa1db9a1ea
jonnii <me@jonnii.com>
2012-07-06 14:48:05 -0400

    docs for bucket repository

--- src/Statr/Storage/IBucketRepository.cs
+++ src/Statr/Storage/IBucketRepository.cs
@@ -2,10 +2,22 @@
 
 namespace Statr.Storage
 {
+    /// <summary>
+    /// The bucket repository is responsible for managing the available buckets
+    /// </summary>
     public interface IBucketRepository
     {
+        /// <summary>
+        /// Lists the available buckets
+        /// </summary>
+        /// <returns>The buckets available</returns>
         IEnumerable<Bucket> List();
 
+        /// <summary>
+        /// Gets a bucket by bucket reference
+        /// </summary>
+        /// <param name="bucketReference">The bucket reference to get</param>
+        /// <returns>A bucket</returns>
         Bucket Get(BucketReference bucketReference);
     }
 }
