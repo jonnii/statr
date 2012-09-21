3063bfc7c5894f15772f5465d92e16ef02c27cf6
jonnii <me@jonnii.com>
2012-09-21 15:04:38 -0400

    get a simple graph working in real time.

--- src/Statr.Web/Scripts/main.js
+++ src/Statr.Web/Scripts/main.js
@@ -63,7 +63,6 @@
                 };
 
                 subscriber.receive = function (message) {
-                    console.log(message);
                     that.get('dataPoints').pushObject(message);
                 };
 
@@ -79,7 +78,59 @@
     });
 
     App.BucketView = Em.View.extend({
-        templateName: 'bucket'
+        chart: null,
+        templateName: 'bucket',
+        didInsertElement: function () {
+            console.log("setting up graph");
+            var that = this;
+            nv.addGraph(function () {
+                var data = [{
+                    values: [],
+                    key: 'Things',
+                    color: '#ff7f0e'
+                }];
+
+                that.chart = nv.models.lineChart();
+                that.chart.xAxis
+                    .axisLabel("Time")
+                    .tickFormat(d3.format(',r'));
+
+                that.chart.yAxis
+                    .axisLabel("Value")
+                    .tickFormat(d3.format('.02f'));
+
+                d3.select("#data-points-graph")
+                    .datum(data)
+                    .transition().duration(500).call(that.chart);
+
+                return that.chart;
+            });
+        },
+        valueDidChange: function () {
+            var chart = this.get('chart');
+
+            if (chart == null) {
+                return;
+            }
+
+            var dataPoints = this.get('controller')
+                .get('dataPoints');
+
+            var values = $.map(dataPoints, function (d) {
+                return { x: d.TimeStamp, y: d.Value };
+            });
+
+            var data = [{
+                values: values,
+                key: 'Things',
+                color: '#ff7f0e'
+            }];
+
+            d3.select('#data-points-graph')
+                .datum(data)
+                .transition().duration(500)
+                .call(chart);
+        }.observes('controller.dataPoints.@each')
     });
 
     App.Bucket = DS.Model.extend({
